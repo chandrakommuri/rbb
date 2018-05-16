d70d1d3d89c09863288b05aa5ad7274b2f8e6686
chandrakommuri <38568507+chandrakommuri@users.noreply.github.com>
2018-05-16 14:25:40 +0530

    Student Registration

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Student Regidtration Form</title>
+	<script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript">
+	
+	</script>
+</head>
+<body>
+	<form name="registration" method="post" action="register.php">
+	
+	</form>
+</body>
+</html>
